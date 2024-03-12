fb5aa5c68f5f1ac1a251061e7dc344f0fa632ba1
Xavi Guasch <xaviguasch@gmail.com>
2024-03-12 15:46:08 +0100

    added models/schemas for Property and User, and fetched the properties from the database through the /api/properties point

--- app/page.jsx
+++ app/page.jsx
@@ -2,11 +2,7 @@ import Hero from '@/components/Hero'
 import InfoBoxes from '@/components/InfoBoxes'
 import HomeProperties from '@/components/HomeProperties'
 
-import connectDB from '@/config/database'
-
-const HomePage = async () => {
-  await connectDB()
-
+const HomePage = () => {
   return (
     <>
       <Hero />
