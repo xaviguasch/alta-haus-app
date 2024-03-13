cb45378a1972705bf365e6f23f8d2f06e3b11f33
Xavi Guasch <xaviguasch@gmail.com>
2024-03-13 10:52:57 +0100

    added dynamic fetching from properties/id page

--- utils/requests.js
+++ utils/requests.js
@@ -1,5 +1,6 @@
 const apiDomain = process.env.NEXT_PUBLIC_API_DOMAIN || null
 
+// Fetch all properties
 async function fetchProperties() {
   try {
     // Handle the case where the domain is not available yet
@@ -21,4 +22,25 @@ async function fetchProperties() {
   }
 }
 
-export { fetchProperties }
+// Fetch single property
+async function fetchProperty(id) {
+  try {
+    // Handle the case where the domain is not available yet
+    if (!apiDomain) {
+      return null
+    }
+
+    const res = await fetch(`${apiDomain}/properties/${id}`)
+
+    if (!res.ok) {
+      throw new Error('Failed to fetch data')
+    }
+
+    return res.json()
+  } catch (error) {
+    console.log(error)
+    return null
+  }
+}
+
+export { fetchProperties, fetchProperty }
