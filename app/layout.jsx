047ffe53aba8ad228b5f48bbbe8278266955ef58
Xavi Guasch <xaviguasch@gmail.com>
2024-03-14 13:05:34 +0100

    wrapped AuthProvider around the app in the layout

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,6 @@
 import Navbar from '@/components/Navbar'
 import Footer from '@/components/Footer'
+import AuthProvider from '@/components/AuthProvider'
 
 import '@/assets/styles/globals.css'
 
@@ -11,13 +12,15 @@ export const metadata = {
 
 const MainLayout = ({ children }) => {
   return (
-    <html lang='en'>
-      <body>
-        <Navbar />
-        <main>{children}</main>
-        <Footer />
-      </body>
-    </html>
+    <AuthProvider>
+      <html lang='en'>
+        <body>
+          <Navbar />
+          <main>{children}</main>
+          <Footer />
+        </body>
+      </html>
+    </AuthProvider>
   )
 }
 
