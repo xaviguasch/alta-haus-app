b6e3dafacc82a0244e37a634c430bb2da0833d97
Xavi Guasch <xaviguasch@gmail.com>
2024-03-15 10:32:34 +0100

    added small adjustment to links in the Navbar menu

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -193,6 +193,9 @@ const Navbar = () => {
                       role='menuitem'
                       tabIndex='-1'
                       id='user-menu-item-0'
+                      onClick={() => {
+                        setIsProfileMenuOpen(false)
+                      }}
                     >
                       Your Profile
                     </Link>
@@ -202,6 +205,9 @@ const Navbar = () => {
                       role='menuitem'
                       tabIndex='-1'
                       id='user-menu-item-2'
+                      onClick={() => {
+                        setIsProfileMenuOpen(false)
+                      }}
                     >
                       Saved Properties
                     </Link>
