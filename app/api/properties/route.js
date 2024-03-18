b6bce76aa2b0b90cb3ae4660be7eb481a96b4b2e
Xavi Guasch <xaviguasch@gmail.com>
2024-03-18 15:56:41 +0100

    correctly wired POST route /api/properties that submits a property to the database

--- app/api/properties/route.js
+++ app/api/properties/route.js
@@ -1,5 +1,6 @@
 import connectDB from '@/config/database'
 import Property from '@/models/Property'
+import { getSessionUser } from '@/utils/getSessionUser'
 
 // GET /api/properties
 export const GET = async (request) => {
@@ -17,6 +18,16 @@ export const GET = async (request) => {
 
 export const POST = async (request) => {
   try {
+    await connectDB()
+
+    const sessionUser = await getSessionUser()
+
+    if (!sessionUser || !sessionUser.userId) {
+      return new Response('User ID is required', { status: 401 })
+    }
+
+    const { userId } = sessionUser
+
     const formData = await request.formData()
 
     // Access all values from amenities and images
@@ -49,17 +60,23 @@ export const POST = async (request) => {
         email: formData.get('seller_info.email'),
         phone: formData.get('seller_info.phone'),
       },
-      images,
+      owner: userId,
+      // images,
     }
 
     console.log(propertyData)
 
-    return new Response(
-      JSON.stringify({
-        message: 'Success',
-      }),
-      { status: 200 }
-    )
+    const newProperty = new Property(propertyData)
+    await newProperty.save()
+
+    return Response.redirect(`${process.env.NEXTAUTH_URL}/properties/${newProperty._id}`)
+
+    // return new Response(
+    //   JSON.stringify({
+    //     message: 'Success',
+    //   }),
+    //   { status: 200 }
+    // )
   } catch (error) {
     return new Response('Failed to add property', { status: 500 })
   }
