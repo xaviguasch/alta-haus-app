7236b725fbfb0904b29e2483b5337b3a52d4fbc5
Xavi Guasch <xaviguasch@gmail.com>
2024-03-20 11:08:40 +0100

    successfully deleted properties

--- app/api/properties/[id]/route.js
+++ app/api/properties/[id]/route.js
@@ -1,5 +1,7 @@
 import connectDB from '@/config/database'
 import Property from '@/models/Property'
+import { getSessionUser } from '@/utils/getSessionUser'
+import cloudinary from '@/config/cloudinary'
 
 // GET /api/properties/:id
 export const GET = async (request, { params }) => {
@@ -16,5 +18,55 @@ export const GET = async (request, { params }) => {
   } catch (error) {
     console.log(error)
     return new Response('Something went wrong', { status: 500 })
+  }
+}
+
+// DELETE /api/properties/:id
+export const DELETE = async (request, { params }) => {
+  try {
+    const propertyId = params.id
+
+    const sessionUser = await getSessionUser()
+
+    // Check for session
+    if (!sessionUser || !sessionUser.userId) {
+      return new Response('User ID is required', { status: 401 })
+    }
+
+    const { userId } = sessionUser
+
+    await connectDB()
+
+    const property = await Property.findById(propertyId)
+
+    if (!property) return new Response('Property Not Found', { status: 404 })
+
+    // Verify ownership
+    if (property.owner.toString() !== userId) {
+      return new Response('Unauthorized', { status: 401 })
+    }
+
+    // extract public id's from image url in DB
+    const publicIds = property.images.map((imageUrl) => {
+      const parts = imageUrl.split('/')
+      return parts.at(-1).split('.').at(0)
+    })
+
+    // Delete images from Cloudinary
+    if (publicIds.length > 0) {
+      for (let publicId of publicIds) {
+        await cloudinary.uploader.destroy('propertypulse/' + publicId)
+      }
+    }
+
+    // Proceed with property deletion
+    await property.deleteOne()
+
+    return new Response('Property Deleted', {
+      status: 200,
+    })
+  } catch (error) {
+    console.log(error)
+    return new Response('Something Went Wrong', { status: 500 })
   }
 }
